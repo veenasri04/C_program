3b7b57f3e5ba4d5a39f23adb610dcc2a069fd757
Veenasri <140997687+veenasri04@users.noreply.github.com>
2023-08-08 16:08:08 +0530

    Add files via upload

---
+++ areaofrectangle.c
@@ -0,0 +1,12 @@
+//Write a program to find the area of the rectangle
+
+#include<stdio.h>
+main()
+{
+	int l;
+	float w,r;
+	printf("enter l,w:\n");
+	scanf("%d%f",&l,&w);
+	r=l*w;
+	printf("%f\n",r);
+}
